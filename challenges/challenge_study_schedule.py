c9bc89284890d890f57d967cbf5aa03e30cc48ed
jvieirah <me.contrate.aero@gmail.com>
2023-03-23 23:47:26 -0300

    refactor to fix issue

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,6 +3,8 @@ def study_schedule(permanence_period, target_time):
     max_students = 0
     for period in permanence_period:
         try:
+            if type(period[0]) != int or type(period[1]) != int:
+                return None
             if period[0] <= target_time <= period[1]:
                 max_students  += 1
         except TypeError:
