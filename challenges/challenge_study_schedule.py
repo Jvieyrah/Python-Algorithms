97a4b4b1d319a1e75a66179915062594c037f65e
jvieirah <me.contrate.aero@gmail.com>
2023-03-23 22:22:39 -0300

    comit inicial, exercicio1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    max_students = 0
+        for period in permanence_period:
+            if period[0] <= target_time < period[1]:
+                students += 1
+
+    return max_students
