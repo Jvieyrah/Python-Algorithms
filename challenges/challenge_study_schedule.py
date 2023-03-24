e67e63c019505b20a159c4a916b3b064e28bc604
jvieirah <me.contrate.aero@gmail.com>
2023-03-24 00:08:41 -0300

    refactor to fix issue

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    if target_time is None:
+        return None
     max_students = 0
     for period in permanence_period:
-        try:
-            if type(period[0]) != int or type(period[1]) != int:
-                return None
-            if period[0] <= target_time <= period[1]:
-                max_students  += 1
-        except TypeError:
+        if type(period[0]) != int or type(period[1]) != int:
             return None
+        if period[0] <= target_time <= period[1]:
+            max_students += 1
+    
     return max_students
