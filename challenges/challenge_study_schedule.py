c8b9a9df392a31d9604017f61bd323d35dc871d9
jvieirah <me.contrate.aero@gmail.com>
2023-03-23 22:35:38 -0300

    comit inicial, exercicio1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,8 +4,8 @@ def study_schedule(permanence_period, target_time):
         return None
 
     max_students = 0
-        for period in permanence_period:
-            if period[0] <= target_time < period[1]:
-                students += 1
+    for period in permanence_period:
+        if period[0] <= target_time < period[1]:
+            students += 1
 
     return max_students
