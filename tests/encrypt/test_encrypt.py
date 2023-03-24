558417971fbd0abb0271fac2703b5c6243e4c5c2
jvieirah <me.contrate.aero@gmail.com>
2023-03-24 00:19:21 -0300

    teste do exercicio2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message('martes', 1) == 'm_setra'
+
+    assert encrypt_message('martes', 2) == 'setr_am'
+
+    assert encrypt_message('martes', 10) == 'setram'
+
+    with raises(TypeError):
+        encrypt_message(0, 0)
+
+    with raises(TypeError):
+        encrypt_message('string', 'int')
