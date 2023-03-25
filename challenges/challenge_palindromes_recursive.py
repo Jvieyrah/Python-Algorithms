51e37210a3a5530cc72e9e1bdb7c4e711c644136
jvieirah <me.contrate.aero@gmail.com>
2023-03-24 22:42:25 -0300

    exercicio 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # Verifica se a string está vazia
+    if len(word) == 0:
+        return False
+    # Verifica se um lado da string é igual a seu indice do lado equivalente oposto
+    elif low_index >= high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+    #repete o processo para o proximo ṕar de indices até concluir.
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    
