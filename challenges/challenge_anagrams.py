eb1eabd7715fab67f32f9a2f07918fcd584862a1
jvieirah <me.contrate.aero@gmail.com>
2023-03-24 23:07:06 -0300

    exercicio 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
+# https://stackoverflow.com/questions/13101468/python-how-to-sort-the-alphabet-in-a-list-without-sorted-functions
+def alptbabetical(lst):
+    if not lst:
+        return []
+    return (quicksort([x for x in lst[1:] if x < lst[0]])
+            + [lst[0]] +
+            quicksort([x for x in lst[1:] if x >= lst[0]]))
+
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    raise NotImplementedError
+
+    first_string = alptbabetical(list(str.lower(first_string)))
+    second_string = alptbabetical(list(str.lower(second_string)))
+
+    if (first_string == '' and second_string == ''):
+        return ("", "", False)
+    return (
+            ''.join(first_ordered),
+            ''.join(second_ordered),
+            first_ordered == second_ordered
+        )
